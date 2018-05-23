a9da3f149bb9c613c7b784339defc2ad4fe12ae7
ashok10103 <ashok10103@gmail.com>
2018-05-23 11:22:31 +0530

    added search query and passed on ingredients to checkout summary via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -111,8 +111,17 @@ class BurgerBuilder extends Component {
 		//     } )
 		//     .catch( error => {
 		//         this.setState( { loading: false, purchasing: false } );
-		//     } );
-		this.props.history.push('/checkout');
+        //     } );
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+        }
+        const queryString = queryParams.join('&');
+
+		this.props.history.push({
+            pathname:'/checkout',
+            search:'?'+queryString,
+        });
 	};
 
 	render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,22 +3,31 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
-    state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
-    }
+	state = {
+		ingredients: {
+			salad: 1,
+			meat: 1,
+			cheese: 1,
+			bacon: 1
+		}
+	};
+	componentDidMount() {
+		const query = new URLSearchParams(this.props.location.search);
 
-    render() {
-        return (
-            <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
-            </div>
-        );
-    }
+		const ingredients = {};
+		for (let param of query.entries()) {
+			ingredients[param[0]] = +param[1];
+		}
+		this.setState({ ingredients: ingredients });
+	}
+
+	render() {
+		return (
+			<div>
+				<CheckoutSummary ingredients={this.state.ingredients} />
+			</div>
+		);
+	}
 }
 
 export default Checkout;
