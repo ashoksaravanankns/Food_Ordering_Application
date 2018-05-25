8740c5d9436e834b13abe6d7bb94178272adac72
ashok10103 <ashok10103@gmail.com>
2018-05-25 11:59:23 +0530

    resolved validation bug

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -114,16 +114,16 @@ class ContactData extends Component {
 			});
 	};
 	checkValidity(value, rules) {
-		let isValid = false;
+		let isValid = true;
 
 		if (rules.required) {
-			isValid = value.trim() !== '';
+			isValid = value.trim() !== '' && isValid;
 		}
 		if (rules.minLength) {
-			isValid = value.length >= rules.minLength;
+			isValid = value.length >= rules.minLength && isValid;
 		}
 		if (rules.maxLength) {
-			isValid = value.length <= rules.maxLength;
+			isValid = value.length <= rules.maxLength && isValid;
 		}
 		return isValid;
 	}
