1caeb67c15ebd58f56381704fb4f2085c84eaa4f
ashok10103 <ashok10103@gmail.com>
2018-05-25 12:37:02 +0530

    dropdown issue resolved

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,7 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: '',
+                value: 'cheapest',
                 valid: true
             }
         },
