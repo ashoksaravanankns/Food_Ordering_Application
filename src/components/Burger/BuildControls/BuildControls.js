037d451d4181a77970732bba484f799185675165
ashok10103 <ashok10103@gmail.com>
2018-05-29 12:44:02 +0530

    added order button only for signed up user

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -24,7 +24,7 @@ const buildControls = (props) => (
         <button 
             className={classes.OrderButton}
             disabled={!props.purchasable}
-            onClick={props.ordered}>ORDER NOW</button>
+            onClick={props.ordered}>{props.isAuth?'ORDER NOW':'SIGNUP TO ORDER'}</button>
     </div>
 );
 
