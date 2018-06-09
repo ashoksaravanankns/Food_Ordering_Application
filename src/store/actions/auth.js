af228b91da218e5d6d9ce1883e6f90d00c7d8e69
ashok10103 <ashok10103@gmail.com>
2018-06-09 19:04:25 +0530

    removed all console.log statements

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -54,7 +54,7 @@ export const auth = (email, password, isSignup) => {
         }
         axios.post(url, authData)
             .then(response => {
-                console.log(response);
+
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
