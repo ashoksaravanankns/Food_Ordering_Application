e4600c896915d7685aae85c7913814e5b0a5bdec
ashok10103 <ashok10103@gmail.com>
2018-06-09 12:16:15 +0530

    added gaurds for routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import React, { Component } from 'react';
-import { Route, Switch ,withRouter} from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Logout from './containers/Auth/Logout/Logout';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
 
 class App extends Component {
-
-	componentDidMount(){
-		this.props.onTryAutoSignUp()
+	componentDidMount() {
+		this.props.onTryAutoSignUp();
 	}
 	render() {
+		let gaurds = (
+			<Switch>
+				<Route path="/auth" component={Auth} />
+				<Route path="/" exact component={BurgerBuilder} />
+				<Redirect to="/" />
+			</Switch>
+		);
+		if (this.props.validUserToken) {
+			gaurds = (
+				<Switch>
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/orders" component={Orders} />
+					<Route path="/logout" component={Logout} />
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</Switch>
+			);
+		}
 		return (
 			<div>
-				<Layout>
-					<Switch>
-						<Route path="/checkout" component={Checkout} />
-						<Route path="/orders" component={Orders} />
-						<Route path="/auth"  component={Auth} />
-						<Route path = '/logout' component = {Logout} />
-						<Route path="/" exact component={BurgerBuilder} />
-					</Switch>
-				</Layout>
+				<Layout>{gaurds}</Layout>
 			</div>
 		);
 	}
 }
 
-const mapDispatchToProps = dispatch =>{
+const mapStateToProps = (state) => {
 	return {
-		onTryAutoSignUp:()=>dispatch(actions.authCheckState())
-	}
-}
+		validUserToken: state.auth.token
+	};
+};
 
-export default withRouter(connect(null,mapDispatchToProps)(App));
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onTryAutoSignUp: () => dispatch(actions.authCheckState())
+	};
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
