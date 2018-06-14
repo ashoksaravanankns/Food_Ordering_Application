6e9f7e971703381c7352b3af663df6b741ebf3ac
ashok10103 <ashok10103@gmail.com>
2018-06-14 22:42:55 +0530

    added authentication tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {configure,shallow} from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
+configure({ adapter: new Adapter() });
+let wrapper;
+beforeEach(() => {
+	wrapper = shallow(<NavigationItems />);
+});
 
-configure({ adapter :new Adapter()})
+describe('<NavigationItems />', () => {
+	// test for non authenticated users
+	it('should have to render 2 <NavigationItem /> component if not authenticated', () => {
+		expect(wrapper.find(NavigationItem)).toHaveLength(2);
+	});
 
-describe('<NavigationItems />',()=>{
-    it('should have to render 2 <NavigationItem /> component if not authenticated',()=>{
-       const wrapper= shallow(<NavigationItems />);
-       expect(wrapper.find(NavigationItem)).toHaveLength(2);
-    })
-})
+	it('should have to render 3 <NavigationItem /> component if authenticated', () => {
+		// wrapper = shallow(<NavigationItems isAuthenticated />);
+		wrapper.setProps({ isAuthenticated: true });
+		expect(wrapper.find(NavigationItem)).toHaveLength(3);
+	});
+});
