df95c50659d483280001d14517eb3fa959e526a3
ashok10103 <ashok10103@gmail.com>
2018-07-12 18:21:49 +0530

    added new generator for logouSaga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,11 +24,11 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-	localStorage.removeItem('token');
-	localStorage.removeItem('expirationDate');
-	localStorage.removeItem('userId');
+	// localStorage.removeItem('token');
+	// localStorage.removeItem('expirationDate');
+	// localStorage.removeItem('userId');
 	return {
-		type: actionTypes.AUTH_LOGOUT
+		type: actionTypes.AUTH_INITIATE_LOGOUT
 	};
 };
 
@@ -81,11 +81,11 @@ export const authCheckState = () => {
 	return (dispatch) => {
 		const token = localStorage.getItem('token');
 		if (!token) {
-			// dispatch(logout());
+			dispatch(logout());
 		} else {
 			const expirationDate = new Date(localStorage.getItem('expirationDate'));
 			if (expirationDate <= new Date()) {
-				// dispatch(logout());
+				dispatch(logout());
 			} else {
 				const userId = localStorage.getItem('userId');
 				dispatch(authSuccess(token, userId));
