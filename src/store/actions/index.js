16d6c9c8700d1cf4d5c8c35976b455f73c4162d2
ashok10103 <ashok10103@gmail.com>
2018-07-12 21:56:57 +0530

    removed all actions from orders and moved to saga middleware

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,23 +1,29 @@
 export {
-    addIngredient,
-    removeIngredient,
-    initIngredients,
-    setIngredients,
-    fetchIngredientsFailed,
+	addIngredient,
+	removeIngredient,
+	initIngredients,
+	setIngredients,
+	fetchIngredientsFailed
 } from './burgerBuilder';
 export {
-    purchaseBurger,
-    purchaseInit,
-    fetchOrders
+	purchaseBurger,
+	purchaseInit,
+	fetchOrders,
+	purchaseBurgerStart,
+	purchaseBurgerSuccess,
+	purchaseBurgerFail,
+	fetchOrdersStart,
+	fetchOrdersSuccess,
+	fetchOrdersFail,
 } from './order';
 export {
-    auth,
-    logout,
-    setAuthRedirectPath,
-    authCheckState,
-    logoutSucceed,
-    authStart,
-    authSuccess,
-    authFail,
-    checkAuthTimeout,
+	auth,
+	logout,
+	setAuthRedirectPath,
+	authCheckState,
+	logoutSucceed,
+	authStart,
+	authSuccess,
+	authFail,
+	checkAuthTimeout
 } from './auth';
